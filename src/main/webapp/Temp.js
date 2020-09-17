306ac61fb62bb0ab00e11d972785df711a93db4a
ggf <guogenfang@gmail.com>
2020-09-17 18:56:19 +0800

    Update Temp.js

--- src/main/webapp/Temp.js
+++ src/main/webapp/Temp.js
@@ -2,44 +2,44 @@ var $route = {};
 var baseUrl = 'http://localhost:8080/?';
 $route.IMenuService = {
 	'getMenuList' : function(success, error) {
-		var _url = 'i=com.sifayun.platform.service.api.IMenuService&m=getMenuList';
+		var _url = 'i=com.ggf.platform.service.api.IMenuService&m=getMenuList';
 		post(_url, {
 			'p' : JSON.stringify([])
 		}, success, error);
 	},
-	'_interface' : 'com.sifayun.platform.service.api.IMenuService'
+	'_interface' : 'com.ggf.platform.service.api.IMenuService'
 }
 
 $route.IUserService = {
 	'all' : function(success, error) {
-		var _url = 'i=com.sifayun.platform.service.api.IUserService&m=all';
+		var _url = 'i=com.ggf.platform.service.api.IUserService&m=all';
 		post(_url, {
 			'p' : JSON.stringify([])
 		}, success, error);
 	},
 	'delete' : function(id, success, error) {
-		var _url = 'i=com.sifayun.platform.service.api.IUserService&m=delete';
+		var _url = 'i=com.ggf.platform.service.api.IUserService&m=delete';
 		post(_url, {
 			'p' : JSON.stringify([ id ])
 		}, success, error);
 	},
 	'save' : function(user, success, error) {
-		var _url = 'i=com.sifayun.platform.service.api.IUserService&m=save';
+		var _url = 'i=com.ggf.platform.service.api.IUserService&m=save';
 		post(_url, {
 			'p' : JSON.stringify([ user ])
 		}, success, error);
 	},
 	'findById' : function(id, success, error) {
-		var _url = 'i=com.sifayun.platform.service.api.IUserService&m=findById';
+		var _url = 'i=com.ggf.platform.service.api.IUserService&m=findById';
 		post(_url, {
 			'p' : JSON.stringify([ id ])
 		}, success, error);
 	},
 	'login' : function(username, password, success, error) {
-		var _url = 'i=com.sifayun.platform.service.api.IUserService&m=login';
+		var _url = 'i=com.ggf.platform.service.api.IUserService&m=login';
 		post(_url, {
 			'p' : JSON.stringify([ username, password ])
 		}, success, error);
 	},
-	'_interface' : 'com.sifayun.platform.service.api.IUserService'
+	'_interface' : 'com.ggf.platform.service.api.IUserService'
 }
